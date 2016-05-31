d90d85998c1942cc27eb130a2eed23bdc0afa65b
Branden Alder <trustyn.skyfall@yahoo.com>
2016-05-31 05:04:04 +0000

    Added Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,10 @@
 Bruit.create(volume: "None")
 Bruit.create(volume: "Quiet")
 Bruit.create(volume: "Loud")
+Technologist.create(firstname: "Sue", lastname: "D")
+Technologist.create(firstname: "Bill", lastname: "O")
+Technologist.create(firstname: "Howard", lastname: "H")
+Technologist.create(firstname: "Sally", lastname: "P")
+User.create(username: "Trustyn", firstname: "Branden", lastname: "Alder",
+            email: "blah@v.com", password: "Password1",
+            password_confirmation: "Password1", activated: true)
