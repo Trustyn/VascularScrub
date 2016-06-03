a691a04d32b5f5408ef71f4f708241e979ce1973
Branden Alder <branden.w.alder@gmail.com>
2016-06-03 12:56:56 +0000

    Added new logo, adjusted topbar to be a bit smaller, and started putting comments in css code so I can keep track of what each item does.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,24 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Dropdown menu for bruits on carotid_exam form.
 
 Bruit.create(volume: "None")
 Bruit.create(volume: "Quiet")
 Bruit.create(volume: "Loud")
+
+#Random list of technologist for testing purposes.
 Technologist.create(firstname: "Sue", lastname: "D")
 Technologist.create(firstname: "Bill", lastname: "O")
 Technologist.create(firstname: "Howard", lastname: "H")
 Technologist.create(firstname: "Sally", lastname: "P")
+
+#Default users just in case.
+    #Branden Alder's
 User.create(username: "Trustyn", firstname: "Branden", lastname: "Alder",
-            email: "blah@v.com", password: "Password1",
+            email: "trustyn@vascularscrub.com", password: "Password1",
+            password_confirmation: "Password1", activated: true)
+    #Chad Greene's
+User.create(username: "Chedder", firstname: "Chad", lastname: "Greene",
+            email: "chedder@vascularscrub.com", password: "Password1",
             password_confirmation: "Password1", activated: true)
